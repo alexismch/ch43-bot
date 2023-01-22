dbade384ec9b8b13917b374fbbc4a612d75f9fc8
Alexis Michiels <>
2023-01-22 15:13:50 +0100

    chore(husky & lint): init

--- apps/front/pages/_app.tsx
+++ apps/front/pages/_app.tsx
@@ -3,16 +3,16 @@ import Head from 'next/head';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Welcome to front!</title>
-      </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
-    </>
-  );
+   return (
+      <>
+         <Head>
+            <title>Welcome to front!</title>
+         </Head>
+         <main className="app">
+            <Component {...pageProps} />
+         </main>
+      </>
+   );
 }
 
 export default CustomApp;
