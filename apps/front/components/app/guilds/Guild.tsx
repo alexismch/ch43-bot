960056b68d52ae0c5b7167a82394b2d06533464b
alexismch <pro@alexismch.be>
2023-02-16 23:56:47 +0100

    feat(front: guilds): add style

--- apps/front/components/app/guilds/Guild.tsx
+++ apps/front/components/app/guilds/Guild.tsx
@@ -1,32 +1,29 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Guild = ({ guild }) => {
-   const { push } = useRouter();
-
    return (
-      <div>
-         <span>
-            {guild.icon && (
-               <Image
-                  src={guild.icon}
-                  alt={guild.name}
-                  width={50}
-                  height={50}
-               />
-            )}
-         </span>
-         <span>{guild.name}</span>
-         <span>
-            <button
-               className="login-button"
-               onClick={() => push(`/guilds/${guild.id}`)}>
-               Details
-            </button>
-         </span>
+      <div className="w-60 h-44 shadow-md bg-white dark:bg-dark-dark-grey rounded h-fit overflow-hidden flex flex-col justify-between">
+         {guild.icon && (
+            <Image
+               src={guild.icon}
+               alt={guild.name}
+               width={50}
+               height={50}
+               className="rounded-full mx-auto mt-4"
+            />
+         )}
+         <p className="w-max text-xl mx-auto text-light-black dark:text-dark-white">
+            {guild.name}
+         </p>
+         <Link
+            href={`/guilds/${guild.id}`}
+            className="block w-full bg-none px-3.5 py-2.5 text-sm font-semibold text-light-primary dark:text-dark-primary border-t-[1px] border-light-primary dark:border-dark-primary hover:bg-light-primary dark:hover:bg-dark-primary hover:text-light-white dark:hover:text-dark-dark-grey">
+            Details
+         </Link>
       </div>
    );
 };
--- apps/front/components/app/guilds/Guilds.tsx
+++ apps/front/components/app/guilds/Guilds.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { getUserGuilds } from '../../../utils/getters';
 import Guild from './Guild';
 import Unauthorized from '../../errors/Unauthorized';
+import Link from 'next/link';
 
 const Guilds = async ({ user }) => {
    const guilds = await getUserGuilds(user as { email: string });
@@ -11,9 +12,22 @@ const Guilds = async ({ user }) => {
 
    return (
       <div>
-         {guilds.map((guild) => (
-            <Guild key={guild.id} guild={guild} />
-         ))}
+         <h1 className="mb-8 sm:mb-16 text-3xl sm:text-5xl font-bold text-light-primary dark:text-dark-primary">
+            Your administrable guilds
+         </h1>
+         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 place-items-center">
+            {guilds.map((guild) => (
+               <Guild key={guild.id} guild={guild} />
+            ))}
+            <div className="w-60 h-44 shadow-md bg-white dark:bg-dark-dark-grey rounded h-fit overflow-hidden flex justify-center items-center">
+               <Link
+                  href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&permissions=8&scope=bot`}
+                  target="_blank"
+                  className="text-light-primary dark:text-dark-primary">
+                  +
+               </Link>
+            </div>
+         </div>
       </div>
    );
 };
